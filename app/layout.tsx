b8401fef1984ae204868891ebef28e8fec66f97b
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-05-22 17:15:01 -0400

    Default Theme == Default mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,8 +51,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <InviteCodeProvider>
