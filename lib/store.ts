68aeee548a3077f2ae388397daab3264236799b9
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-09-01 21:40:54 -0400

    Audit /lib folder

--- lib/store.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-
-export const useStore = create()(
-  persist(
-    (set) => ({}),
-    { name: "store", skipHydration: true },
-  ),
-);
